4fff582d911e17bca11d3646a657e99fb3468851
huxichun <huxichun163@163.com>
2025-12-05 16:52:16 +0800

    feat: 集成@tanstack/devtools-vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import { devtools } from '@tanstack/devtools-vite'
 
 const config = defineConfig({
   plugins: [
+    devtools(),
     nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
